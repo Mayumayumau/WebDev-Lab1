9ce517c98275da429e8e1fe52d93da5cca640bab
Maya Sidorova <mayasidorova@yahoo.com>
2022-04-08 16:19:35 +0800

    fix read_image_files() in net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -26,7 +26,7 @@ resnet = ResNet50V2(include_top=True, weights='imagenet', input_tensor=visible2,
 
 
 def read_image_files(files_max_count, dir_name):
-    files = os.listdir(dir_name)
+    files = [item.name for item in os.scandir(dir_name) if item.is_file()]
     files_count = files_max_count
     if (files_max_count > len(files)):
         files_count = len(files)
